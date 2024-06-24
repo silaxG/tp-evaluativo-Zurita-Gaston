4ccd6118893b13b565bd8e42854f9f73d75c1193
silaxG <gastonizurita@gmail.com>
2024-06-24 10:54:16 -0300

    agregando material angular y vinculando el proyecto a firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//modulo que define el modulo raiz
+import { AppRoutingModule } from './app-routing.module';
+//importacion de base de datos
+import { enviroment } from './enviroments/enviroment';
+import {AngularFireModule} from '@angular/fire/compat'; //trabaja con las colecciones de la informacion
+import {AngularFireAuthModule} from '@angular/fire/compat/auth'; //trabaja con la autentificacion
+import {AngularFireStorageModule} from '@angular/fire/compat/storage' //trabaja con imagenes y archivos
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //vinculacion del proyecto con firebase
+    AngularFireModule.initializeApp(enviroment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/enviroments/enviroment.ts
@@ -0,0 +1,13 @@
+export const enviroment = {
+    production: false,
+    //el proyecto de github se encontrara en privado porque esto esta informacion es delicada
+    firebaseConfig: {
+        apiKey: "AIzaSyAJHpj8B3vP0VdZZ3RvcrKfMPT4H0loKjA",
+        authDomain: "tpevaluativo-giz.firebaseapp.com",
+        projectId: "tpevaluativo-giz",
+        storageBucket: "tpevaluativo-giz.appspot.com",
+        messagingSenderId: "1077130931622",
+        appId: "1:1077130931622:web:0cb7816c8fe4f81ebff367"
+    }
+
+}
