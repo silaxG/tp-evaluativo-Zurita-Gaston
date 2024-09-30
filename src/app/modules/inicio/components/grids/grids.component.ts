8c1b8668100b25c6f38013ead32564379395d46f
silaxG <gastonizurita@gmail.com>
2024-09-30 09:00:55 -0300

    modificacion de pagina de inicio añadiendo 2 imagenes mas y colocando mas responsividad a la pagina

--- src/app/modules/inicio/components/grids/grids.component.ts
+++ src/app/modules/inicio/components/grids/grids.component.ts
@@ -12,9 +12,11 @@ export class GridsComponent {
     { text: 'Amigurumis recien fabricados\n para su venta', cols: 4, rows: 1, imageUrl: '', class: 'solido' },
     { text: '', cols: 4, rows: 4, imageUrl: '', class: 'parallax' },
     { text: 'Revise nuestro catalogo', cols: 4, rows: 2, imageUrl: '', class: 'title', link:'/producto' },
-    { text: '', cols: 4, rows: 4, imageUrl: '/assets/carousel1.jpeg', class: '' },
-    { text: 'falta imagen', cols: 1, rows: 2, imageUrl: '/assets/carousel2.jpeg', class: 'meow' },
-    { text: 'falta imagen', cols: 1, rows: 2, imageUrl: '/assets/carousel3.jpeg', class: 'meow'},
+    { text: '', cols: 4, rows: 4, imageUrl: '/assets/carousel1.jpeg', class: 'meow' },
+    { text: '', cols: 1, rows: 2, imageUrl: '/assets/carousel2.jpeg', class: 'meow' },
+    { text: '', cols: 1, rows: 2, imageUrl: '/assets/carousel4.jpeg', class: 'meow'},
+    { text: '', cols: 1, rows: 2, imageUrl: '/assets/carousel3.jpeg', class: 'meow'},
+    { text: '', cols: 1, rows: 2, imageUrl: '/assets/carousel5.jpeg', class: 'meow'},
     // Otros tiles...
   ];
   
